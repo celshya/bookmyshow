c149061150db3676e5ed3ba1a19f3342342a8367
Celshya <celshya2020@gmail.com>
2024-09-23 19:21:44 +0530

    readme file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 
-import { useState } from 'react';
+
 import './App.css';
 import data from "./data.json"
 import BookingGrid from './components/BookingGrid';
 function BookMyShow() {
-  const [bookingdata,setBookingData] = useState(data)
+  
   return (
     <div className="Bookmyshow">
    
-    <BookingGrid bookingdata = {bookingdata}/>
+    <BookingGrid bookingdata = {data}/>
     </div>
   );
 }
